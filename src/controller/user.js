d1d5f38a8242ed21a19fba013b30cc50b5f8f6b7
xiaoguoguo <1662106538@qq.com>
2021-01-15 17:44:49 +0800

    feat:密码md5加密

--- src/controller/user.js
+++ src/controller/user.js
@@ -1,6 +1,9 @@
 const { exec, escape } = require("../db/mysql");
+const { genPassword } = require('../util/cryp');
 
 const login = (username, password) => {
+  // 密码加密
+  password = genPassword(password);
   username = escape(username);
   password = escape(password);
   const sql = `select username, realname from users where username=${username} and password=${password}`;
---
+++ src/util/cryp.js
@@ -0,0 +1,21 @@
+const crypto = require('crypto');
+
+// 密匙
+const SECRET_KEY = "WJiol_8776#";
+
+// md5加密
+function md5(content) {
+    let md5 = crypto.createHash('md5');
+    return md5.update(content).digest('hex');
+}
+
+// 加密函数
+function genPassword(password) {
+    const str = `password=${password}&key=${SECRET_KEY}`;
+
+    return md5(str);
+}
+console.log(genPassword('a111111'));
+module.exports = {
+    genPassword
+}
